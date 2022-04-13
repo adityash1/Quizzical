d918656e457733a9fe85e10390e7b9a2fe51474d
adityash1 <aditya167411@gmail.com>
2022-04-13 11:56:47 +0530

    setup startpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+import StartPage from "./components/StartPage"
+
+const App = () => {
+  const [quiz, setQuiz] = useState(false);
+
+  const startQuiz = () => {
+    setQuiz(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="yellow-blob"></div>
+      <div className="blue-blob"></div>
+      {!quiz && <StartPage onClick={startQuiz}/>}
     </div>
   );
 }
