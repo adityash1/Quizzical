c1de6f9a5e8b7221e7232bad55b4a870561398ea
adityash1 <aditya167411@gmail.com>
2022-04-13 20:57:18 +0530

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import StartPage from "./components/StartPage"
+import QuizPage from "./components/QuizPage"
 
 const App = () => {
   const [quiz, setQuiz] = useState(false);
+  const [quizData, setQuizData] = useState([])
+
+  useEffect(() => {
+    if (quiz) {
+    axios.get('https://opentdb.com/api.php?amount=5&type=multiple')
+      .then(res => {
+        setQuizData(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }}, [quiz]);
 
   const startQuiz = () => {
     setQuiz(true);
@@ -14,6 +28,7 @@ const App = () => {
       <div className="yellow-blob"></div>
       <div className="blue-blob"></div>
       {!quiz && <StartPage onClick={startQuiz}/>}
+      {quiz && <QuizPage quizData={quizData}/>}
     </div>
   );
 }
